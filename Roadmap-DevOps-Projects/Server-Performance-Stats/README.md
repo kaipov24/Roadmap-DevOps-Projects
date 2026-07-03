26cb3863d1ce4f475869b45d86654d31a6e57203
kaipov24 <kaipovich24@gmail.com>
2026-07-03 16:13:18 +0600

    Server Performance Stats

---
+++ Roadmap-DevOps-Projects/Server-Performance-Stats/README.md
@@ -0,0 +1,30 @@
+# Server Performance Stats
+
+- Total CPU usage
+- Memory usage
+- Disk usage
+- Top processes by CPU usage
+- Top processes by memory usage
+
+## Usage
+
+Clone this repository:
+
+```bash
+git clone https://github.com/kaipov24/devops-Projects.git
+cd Server-Performance-Stats
+```
+
+Give the script execute permission:
+
+```bash
+chmod +x server-stats.sh
+```
+
+Run it:
+
+```bash
+./server-stats.sh
+```
+
+Project from [roadmap.sh Server Stats](https://roadmap.sh/projects/server-stats).
---
+++ Roadmap-DevOps-Projects/Server-Performance-Stats/server-stats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Total CPU usage
+top -bn1 | awk '/Cpu\(s\)/ {print 100 - $8 "%"}'
+
+#Total memory usage (Free vs Used including percentage)
+
+free | awk 'NR == 2 {printf "Free %.0f%% vs Used %.0f%%\n", $3*100/$2, $4*100/$2}'
+
+#Total disk usage (Free vs Used including percentage)
+df -h --total | tail -1 | awk '{printf "Free %.0f%% vs Used %.0f%%\n", $4*100/$2, $3*100/$2}'
+
+#Top 5 processes by CPU usage
+ps -aux --sort=-%cpu | head -5
+
+#Top 5 processes by memory usage
+ps -aux --sort=-%mem | head -5ß
